f4c064ae48c68a2f08e024e2d8026609d3dc246c
Leen27 <m13840918032@163.com>
2023-12-19 18:11:21 +0800

    AI

--- packages/primvue/src/main.ts
+++ packages/primvue/src/main.ts
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import PrimeVue from 'primevue/config';
+// @ts-ignore
+import wind from '@/presets/wind/';
 
 createApp(App).use(PrimeVue, {
-    // unstyled: true
+    unstyled: true,
+    pt: wind
 }).mount('#app')
